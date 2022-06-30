23fcfe0a30673048c19af0bb0a50df7e98a34ddb
Jeret <1600012705@pku.edu.cn>
2022-07-01 00:04:39 +0800

    add some

--- run_training.py
+++ run_training.py
@@ -76,7 +76,7 @@ def main():
         model = Model(session=session, config=config, batcher=batcher)
         model.build_graph()
         model.load()
-        #model.train()
+        model.train()
 
 if __name__ == '__main__':
     main()
